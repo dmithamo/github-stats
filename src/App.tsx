0630dc2ac64aaa387b51a65d61e0f7195bc76a34
Dennis Mithamo <dennisbmithamo@gmail.com>
2019-10-07 14:31:59 +0300

    chore(setup): initialize app
    - start a CRA app
    - set up pre-commit hooks
    - clean up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <div>
+    <h2>React App</h2>
+    <p>React App in Typescript including linting with eslint</p>
+    <sub>
+      <code>npx create-react-app --use-npm --typescript</code>
+    </sub>
+  </div>
+);
+
+export default App;
